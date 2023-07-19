18e516aa8162a535bc6945c2d76e0f96ae2a0017
dharasoni16 <99190748+dharasoni16@users.noreply.github.com>
2023-07-19 15:32:50 +0200

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+alert('Hello World');
+
+// Defining variables with values
+let myName = 'Bob';
+document.write(myName);
+myName = 'John Doe';
+document.write(myName);
+
+// Mathematical Operation
+let simpleAddition = 2 + 2;
+document.write(simpleAddition);
+let size = 100;
+let doubleSize = size * 2;
+document.write(doubleSize);
+let minSize = doubleSize * 2 - size / 2;
+document.write(minSize);
+
+//Practicing variable
+let favoriteFood='IndianFood';
+document.write(favoriteFood);
