b0718723c1861de506e4c3a22e9b01a56c36a4c9
dharasoni16 <99190748+dharasoni16@users.noreply.github.com>
2023-07-21 18:13:36 +0200

    docs:update js file implements loops and conditionals

--- js/scripts.js
+++ js/scripts.js
@@ -30,3 +30,21 @@ let pokemonList = [
     category: "poison bee",
   },
 ];
+
+//Declaring variables with empty string
+let pokemon = "";
+let text = "";
+
+//looping through the Pokemon list arrays
+for (let i = 0; i < pokemonList.length; i++) {
+  //assigning pokemon variable to an instance of pokemon in pokemonList array
+  pokemon = pokemonList[i];
+
+  //assigning variable a string to print the list of pokemons on screen
+  text = pokemon.name + " (height: " + pokemon.height + ")";
+
+  //ternary condition to check pokemon having biggest height and printing the pokemon list on screen
+  pokemon.height >= 7
+    ? document.write(text + " - Wow, that's big!<br>")
+    : document.write(text + "<br>");
+}
